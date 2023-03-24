306710d13c8975e12ee077c76a11880627206536
Phurin Chawjarusopas <62511523+sunskung@users.noreply.github.com>
2023-03-24 18:32:44 +0700

    save code

--- Consuming API/test.py
+++ Consuming API/test.py
@@ -5,8 +5,8 @@
 # ls -l /usr/bin/python*
 # print('dog')
 
-# import requests
+import requests
 # import selenium
-from bs4 import BeautifulSoup
+# from bs4 import BeautifulSoup
 
 # pip install --upgrade pip
