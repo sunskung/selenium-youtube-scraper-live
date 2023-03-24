b14a41bf3ea2f43b36b55548c4bdd19bee0760ca
Phurin Chawjarusopas <62511523+sunskung@users.noreply.github.com>
2023-03-24 17:47:11 +0700

    Update test.py

---
+++ Consuming API/test.py
@@ -0,0 +1,12 @@
+# python -m pip install requests
+# pip install requests
+# python3 -m pip install requests
+# python -m pip install requests
+# ls -l /usr/bin/python*
+# print('dog')
+
+# import requests
+# import selenium
+from bs4 import BeautifulSoup
+
+# pip install --upgrade pip
