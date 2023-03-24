cae1d8f14484138aa6a63a7fc0a4a28b6c65064e
Phurin Chawjarusopas <62511523+sunskung@users.noreply.github.com>
2023-03-24 19:28:51 +0700

    Update test.py

--- Consuming API/test.py
+++ Consuming API/test.py
@@ -1,11 +1,14 @@
 # python -m pip install requests
 # pip install requests
+# pip install smtplib
+# conda install smtplib
 # python3 -m pip install requests
 # python -m pip install requests
 # ls -l /usr/bin/python*
 # print('dog')
 
-import requests
+# import requests
+import smtplib
 # import selenium
 # from bs4 import BeautifulSoup
 
