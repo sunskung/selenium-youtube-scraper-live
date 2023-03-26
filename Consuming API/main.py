ffbcd4015cfd8b27c0cbe9f1c84781378b84a6cb
Phurin Chawjarusopas <62511523+sunskung@users.noreply.github.com>
2023-03-26 16:27:27 +0700

    Update main.py

--- Consuming API/main.py
+++ Consuming API/main.py
@@ -1,6 +1,22 @@
 # pip install requests
 import requests
 
-requests.get("https://randomuser.me/api/")
+# obj = [[1, 2, 3], list(range(10000))]
+# print(r.repr(obj))
 
-print('dog')
+# response = requests.get("https://randomuser.me/api/")
+# print(response.text)
+
+# response = requests.get("https://api.thedogapi.com/")
+# print(response.text)
+
+response = requests.get("https://api.thedogapi.com/v1/breeds")
+# print(response.text)
+# print(response.text)
+# print(response.text)
+# print(response.text)
+# print(response.text)
+# print(response.text)
+# print(response.text)
+# print(response.text)
+# print(response.text)
